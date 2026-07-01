e967286f4e17a9c2ab0a3b013dcb84e1542c2fd3
ben133290 <benheuser2@gmail.com>
2026-07-01 11:36:46 +0200

    update runs

--- custom_parser.py
+++ custom_parser.py
@@ -6,6 +6,15 @@ import sys
 
 from lab.parser import Parser
 
+def translator_exit(content, props):
+    if "translator_exit_code" in props:
+        if props["translator_exit_code"] == 0:
+            props["translator_success"] = 1
+        else:
+            props["translator_success"] = 0
+    else:
+        props["translator_success"] = 0
+
 class CustomParser(Parser):
     def __init__(self):
         Parser.__init__(self)
@@ -13,4 +22,5 @@ class CustomParser(Parser):
 def get_parser():
     parser = CustomParser()
     parser.add_pattern("translator_exit_code", r"translate exit code: (.+)", type=int, file="run.log")
+    parser.add_function(translator_exit)
     return parser
